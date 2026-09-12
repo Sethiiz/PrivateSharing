8bf684f5efb8ebacbe32b83dd6aa27ca997db76f
Sethiiz <49239698+Sethiiz@users.noreply.github.com>
2026-09-12 03:05:40 -0300

    chore: bump version to 1.0.10 for auto-update flow test

--- client/src/renderer/patchnotes.js
+++ client/src/renderer/patchnotes.js
@@ -14,4 +14,5 @@ TDG.patchNotes = {
   '1.0.7': ['Nova aba de tema em Configurações: troca a cor do app (fundo e botões) na hora.'],
   '1.0.8': ['Botão "Verificar atualizações" em Configurações, pra checar na hora sem esperar.'],
   '1.0.9': ['Log de atualização em arquivo, pra diagnosticar problemas de update mais fácil.'],
+  '1.0.10': ['Teste do fluxo de atualização automática, ponta a ponta.'],
 };
